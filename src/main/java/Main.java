478f82a2803bd36824fa59b07fa365949ee53afd
hfaure <hugo.faure05@gmail.com>
2018-05-31 11:32:04 +0200

    ceci est juste un pretexte pour tester les branches

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		MesMessages monMessage = new MesMessages();
 		System.out.println(monMessage.getMessage1());
+		monMessage.setMessage1("coucou");
+		System.out.println(monMessage.getMessage1());
 	}
 
 }
