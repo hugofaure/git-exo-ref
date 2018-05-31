1c4280d59735de939fd0972b622fb07f359412b4
hfaure <hugo.faure05@gmail.com>
2018-05-31 11:58:24 +0200

    commit sur la master pour tester les conflits

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,15 @@
 
-
 public class Main {
 
 	public static void main(String[] args) {
 		MesMessages monMessage = new MesMessages();
 		System.out.println(monMessage.getMessage1());
 		monMessage.setMessage1("coucou");
-		System.out.println(monMessage.getMessage1());
+		for (int i = 0; i < 10; i++) {
+			System.out.println(monMessage.getMessage1());
+
+		}
+
 	}
 
 }
