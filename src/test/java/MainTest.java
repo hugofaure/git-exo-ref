8bd59680ed162560c840cbe8d9039eec6b3074a4
hfaure <hugo.faure05@gmail.com>
2018-05-31 10:09:38 +0200

    premier depot du src

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,35 @@
+
+
+public class MainTest {
+	
+//	@BeforeEach
+//	public void SetUp() {
+//		ab = new AnneeBissextile();
+//	}
+//	
+//	@ParameterizedTest
+//	@MethodSource("testDuTest")
+//	public void testIsBissextile1(int annee) {
+//		Assertions.assertTrue(ab.IsBissextile(annee));	
+//	}	 
+//	@SuppressWarnings("unused")
+//	private static Stream<Integer> testDuTest() {
+//	        return Stream.of(400,1528,1989,2000,2018);
+//	        		
+//	    }
+//	@ParameterizedTest
+//	@ValueSource(ints= {400,1528,1989,2000,2018})
+//	public void testIsBissextile2(int annee) {
+//		Assertions.assertTrue(ab.IsBissextile(annee));	
+//	}
+//	@ParameterizedTest
+//	@CsvSource({"400","1528","1989","2000","2018"})
+//	public void testIsBissextile3(int annee) {
+//		Assertions.assertTrue(ab.IsBissextile(annee));	
+//	}
+//	@ParameterizedTest
+//	@CsvFileSource(resources="/test.csv")
+//	public void testIsBissextile4(int annee) {
+//		Assertions.assertTrue(ab.IsBissextile(annee));	
+//	}
+}
